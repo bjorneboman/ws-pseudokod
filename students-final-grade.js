07def27b5cb93d833d10bd15a3c78f10703c0598
bjorneboman <gostabohman@gmail.com>
2025-09-24 10:29:27 +0200

    Finishes pseudo code

---
+++ students-final-grade.js
@@ -0,0 +1,16 @@
+// MÅL: Beräkna slutbetyg utefter provresultat och inlämningsuppgifter.
+
+// INPUTS: Provresultat i heltal och antal inlämningsuppgifter.
+
+// RETURN: Slutbetyg som ett heltal från 1 till 100, enligt nivåerna 100, 90, 75 eller 0.
+
+// STEG 1: Kontrollera värdena för provresultat och inlämningsuppgifter.
+
+// STEG 2: Om provresultatet är högre än 90 eller om antalet inlämningsuppgifter är fler än 10 returnera slutbetyget 100.
+
+// STEG 3: Om provresultatet är högre än 75 och om antalet inlämningsuppgifter är minst 5 returnera slutbetyget 90.
+
+// STEG 4: Om provresultatet är högre än 50 och om antalet inlämningsuppgifter är minst 2 returnera slutbetyget 75.
+
+// STEG 5: I alla andra fall returnera slutbetyget 0.
+
